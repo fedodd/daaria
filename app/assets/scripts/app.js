7617b3c03688d3569bbb5fffcd41040008bd7402
fedodd <fedodd.s@gmail.com>
2018-02-25 14:57:39 +0300

    finished project page Adidas + add link on home

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -60,3 +60,14 @@ $(document).ready(function(){
     lazyLoad: 'ondemand'
   });
 });
+
+
+// плавный переход по якорям
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
